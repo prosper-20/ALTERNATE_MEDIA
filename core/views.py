7933bedbc193930c071ad9f05e24c650827bfa0b
prosper-20 <edwardprosper001@gmail.com>
2022-07-15 11:46:50 +0100

    updates

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.generics.views import ListView
+from django.views.generic import ListView
 
 # Create your views here.
 
-class HomeView(ListView):
-    template_name = "core/home.html"
+# class HomeView(ListView):
+#     template_name = "core/home.html"
+
+
+def HomeView(request):
+    return render(request, 'core/home.html')
